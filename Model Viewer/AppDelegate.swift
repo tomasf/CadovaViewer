6a092840680ceb229e8609794f2c29c14433e92b
Tomas Franzén <tomas@tomasf.se>
2024-12-15 19:40:04 +0100

    Initial Commit

---
+++ Model Viewer/AppDelegate.swift
@@ -0,0 +1,29 @@
+//
+//  AppDelegate.swift
+//  Model Viewer
+//
+//  Created by Tomas Wincent Franzén on 2024-12-15.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
---
+++ Model Viewer/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Model Viewer
+//
+//  Created by Tomas Wincent Franzén on 2024-12-15.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
