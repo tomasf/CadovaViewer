475756dc3f3fab9fe8f43a45e9d66532a84cf99c
Tomas Franzén <tomas@tomasf.se>
2026-03-17 00:05:24 +0100

    Set orbit target to click point in QL preview

--- Quick Look Preview/PreviewSceneView.swift
+++ Quick Look Preview/PreviewSceneView.swift
@@ -4,35 +4,41 @@ import ViewerCore
 
 class PreviewSceneView: SCNView {
     weak var modelNode: SCNNode?
-    
-    override func mouseDown(with event: NSEvent) {
-        super.mouseDown(with: event)
-        
-        if let modelNode = modelNode {
-            let worldCenter = modelNode.convertPosition(modelNode.boundingSphere.center, to: nil)
-            defaultCameraController.target = worldCenter
+
+    private func updateOrbitTarget(for event: NSEvent) {
+        guard let modelNode else { return }
+        let localPoint = convert(event.locationInWindow, from: nil)
+
+        if let result = hitTest(localPoint, options: [
+            .searchMode: SCNHitTestSearchMode.all.rawValue as NSNumber,
+            .rootNode: modelNode
+        ]).first {
+            defaultCameraController.target = result.worldCoordinates
+        } else {
+            defaultCameraController.target = xyPlanePoint(forViewPoint: localPoint)
         }
     }
-    
+
+    override func mouseDown(with event: NSEvent) {
+        super.mouseDown(with: event)
+        updateOrbitTarget(for: event)
+    }
+
     override func rightMouseDown(with event: NSEvent) {
         NSEvent.overriddenModifierFlags = .option
         super.mouseDown(with: event)
-        
-        if let modelNode = modelNode {
-            let worldCenter = modelNode.convertPosition(modelNode.boundingSphere.center, to: nil)
-            defaultCameraController.target = worldCenter
-        }
+        updateOrbitTarget(for: event)
     }
-    
+
     override func rightMouseDragged(with event: NSEvent) {
         super.mouseDragged(with: event)
     }
-    
+
     override func rightMouseUp(with event: NSEvent) {
         NSEvent.overriddenModifierFlags = nil
         super.mouseUp(with: event)
     }
-    
+
     override func scrollWheel(with event: NSEvent) {
         if !event.hasPreciseScrollingDeltas {
             NSEvent.overriddenModifierFlags = .option
@@ -42,6 +48,6 @@ class PreviewSceneView: SCNView {
             super.scrollWheel(with: event)
         }
     }
-    
+
     override var acceptsFirstResponder: Bool { true }
 }
